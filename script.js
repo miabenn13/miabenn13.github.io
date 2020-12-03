d7fe2a38b3718def130b34ad9c25debb60ea722c
miabenn13 <70658411+miabenn13@users.noreply.github.com>
2020-12-02 20:31:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+(function(){
+    var words = [
+        ' a graphic designer',
+        ' an artist',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
